9af8daa86b6db91c5ce8aec9f3ae390ecbf28ac5
Luca Perotti <lperotti@ranorex.com>
2020-06-24 16:28:45 +0200

    feat(app): add TodoList component

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TodoList.css';
+
+const TodoList: React.FC = () => (
+  <div className="TodoList" data-testid="TodoList">
+    TodoList Component
+  </div>
+);
+
+export default TodoList;
